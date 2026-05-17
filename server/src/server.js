6b499ab66a1369925281f1c98e08d35b6c78e1b8
Ahamed Yousuf <ahamedyousuf5859@gmail.com>
2026-05-17 07:11:22 +0530

    initial commit

--- server/src/server.js
+++ server/src/server.js
@@ -4,23 +4,19 @@ import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimitter.js";
 import cors from "cors";
-import path from "path";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const __dirname = path.resolve();
 
 // console.log(__dirname);
 
-if(process.env.NODE_ENV !== "production"){
-  app.use(
+app.use(
   cors({
     origin: "http://localhost:5173",
   }),
 );
-}
 
 app.use(express.json()); // middleware used to parse JSON bodies
 app.use(rateLimiter);
@@ -32,14 +28,6 @@ app.use(rateLimiter);
 // })
 
 app.use("/api/tasks", tasksRoutes);
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/dist")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
-  });
-}
 
 connectDB().then(() =>
   app.listen(PORT, () => {
